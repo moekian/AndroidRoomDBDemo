b1a6d16fbc9129b20aa0a26782785bb04d0f312b
Mohammad Kiani <kianimohammad@outlook.com>
2021-02-22 16:17:13 -0500

    add view model class (EmployeeViewModel)

---
+++ app/src/main/java/com/mohammadkiani/androidroomdbdemo/model/EmployeeViewModel.java
@@ -0,0 +1,33 @@
+package com.mohammadkiani.androidroomdbdemo.model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.mohammadkiani.androidroomdbdemo.data.EmployeeRepository;
+
+import java.util.List;
+
+public class EmployeeViewModel extends AndroidViewModel {
+    public EmployeeRepository repository;
+    public final LiveData<List<Employee>> allEmployees;
+
+    public EmployeeViewModel(@NonNull Application application) {
+        super(application);
+
+        repository = new EmployeeRepository(application);
+        allEmployees = repository.getAllEmployees();
+    }
+
+    public LiveData<List<Employee>> getAllEmployees() {return allEmployees;}
+
+    public LiveData<Employee> getEmployee(int id) {return repository.getEmployee(id);}
+
+    public void insert(Employee employee) {repository.insert(employee);}
+
+    public void update(Employee employee) {repository.update(employee);}
+
+    public void delete(Employee employee) {repository.delete(employee);}
+}
