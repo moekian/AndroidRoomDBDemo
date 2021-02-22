697c0c59f912ba0a572f45093804ac94d5ddef01
Mohammad Kiani <kianimohammad@outlook.com>
2021-02-22 12:40:11 -0500

    add second activity along with its layout

--- app/src/main/java/com/mohammadkiani/androidroomdbdemo/MainActivity.java
+++ app/src/main/java/com/mohammadkiani/androidroomdbdemo/MainActivity.java
@@ -2,7 +2,11 @@ package com.mohammadkiani.androidroomdbdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AddEmployeeActivity.class)));
     }
 }
